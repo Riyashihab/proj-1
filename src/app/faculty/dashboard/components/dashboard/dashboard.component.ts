c499f7f9b0666e75fcab1b956187dd919ce7f994
riyashihab <querybaysolutions@gmail.com>
2021-08-02 21:43:01 +0530

    first commit

---
+++ src/app/faculty/dashboard/components/dashboard/dashboard.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FacultyService } from 'src/app/faculty/faculty.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  registerForm!: FormGroup;
+  //imageSrc: string;
+  gridColumns = 3;
+  minDate = new Date(2010, 4, 12); 
+  formdata = new FormData();
+  constructor( private facultyservice:FacultyService,private formBuilder: FormBuilder) { }
+  facultyinfo= {
+    userid:'6102365b9b787a22e02789aa',
+     username : '',
+     lastname: '',
+     user_email : '',
+     password : '',
+     phone_number : '',
+     skills:'',
+     dateofjoining:'',
+     coursehandling:''
+  }
+  ngOnInit(): void {
+
+    
+
+    
+ 
+
+    console.log("display profilesssss");
+
+    let facultyid = localStorage.getItem("userid");
+    console.log(facultyid);
+    this.facultyservice.getUser(facultyid).subscribe((data)=>{
+      this.facultyinfo=JSON.parse(JSON.stringify(data));
+      console.log(this.facultyinfo);
+  })
+  }
+
+  editProfile(){
+    console.log("submittteeeed",this.facultyinfo);
+    alert(" update Success");
+    this.facultyservice.editUser(this.facultyinfo);   
+    console.log("update",this.facultyinfo);
+   // this.router.navigate(['products']);
+  }
+
+  onChangeSelection(selected: string) {
+    var chang = parseInt(selected);
+    console.log(chang);
+}
+
+
+// onFileChange(event) {
+//   const reader = new FileReader();
+  
+//   if(event.target.files && event.target.files.length) {
+//     const [file] = event.target.files;
+//     reader.readAsDataURL(file);
+  
+//     reader.onload = () => {
+ 
+//       this.imageSrc = reader.result as string;
+   
+//       this.myForm.patchValue({
+//         fileSource: reader.result
+//       });
+ 
+//     };
+ 
+//   }
+// }
+}
+
